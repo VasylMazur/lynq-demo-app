52fdecc59c8e8c711e11c4fa79f12478ac4ced4c
RkVishko <vasa8061@gmail.com>
2020-01-28 13:17:42 +0200

    Updates

---
+++ precache-manifest.edad36a61843373358c2d29bea5a45e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902dd6b52578ab8b076d43f8dd3c820d",
+    "url": "/lynqdemo/index.html"
+  },
+  {
+    "revision": "72375c3bca2d6744a807",
+    "url": "/lynqdemo/static/css/main.a50eb33f.chunk.css"
+  },
+  {
+    "revision": "14d4586c0a13371e26da",
+    "url": "/lynqdemo/static/js/2.da676d8e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/lynqdemo/static/js/2.da676d8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72375c3bca2d6744a807",
+    "url": "/lynqdemo/static/js/main.578b867a.chunk.js"
+  },
+  {
+    "revision": "717ed20086b69361c24c",
+    "url": "/lynqdemo/static/js/runtime-main.d4b36fb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lynqdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
