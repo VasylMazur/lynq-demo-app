e59938c885c498f3d62d4e513fca7f3bd9e4669c
RkVishko <vasa8061@gmail.com>
2020-01-28 17:00:46 +0200

    Updates

---
+++ precache-manifest.379fd8a30f6778ae0f5032db45fc5630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d60972bcbeab0cb99124248be19ff74",
+    "url": "/drinks/index.html"
+  },
+  {
+    "revision": "504196e1f512c28a73fa",
+    "url": "/drinks/static/css/main.a50eb33f.chunk.css"
+  },
+  {
+    "revision": "14d4586c0a13371e26da",
+    "url": "/drinks/static/js/2.da676d8e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/drinks/static/js/2.da676d8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "504196e1f512c28a73fa",
+    "url": "/drinks/static/js/main.bd320d92.chunk.js"
+  },
+  {
+    "revision": "b8460d944a9978c77e02",
+    "url": "/drinks/static/js/runtime-main.f22e9c0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drinks/static/media/logo.5d5d9eef.svg"
+  }
+]);
